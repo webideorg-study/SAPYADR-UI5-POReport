e147f6a2ea8ba93643273c2eb163af73bf3f1f75
KDT <tangdunke@gmail.com>
2019-11-14 17:22:32 +0800

    Initial Commit

---
+++ webapp/controller/POHeader.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("POReport.controller.POHeader", {
+		
+		GoToPOItem: function(oEvent) {
+			//var oContext = oEvent.getSource().getBindingContext();
+			//console.log(oContext.getPath());
+			//alert(oContext.getProperty("LifecycleStatusDescription"));
+		}
+		
+	});
+});
